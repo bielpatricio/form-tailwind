f5961ef9e2f3dcf26754c36570fdaab0ab2da6d1
Gabriel Patrício <gabrieltp087@gmail.com>
2024-03-02 17:09:06 -0300

    Dark mode

--- src/components/Form/Select/SelectItem.tsx
+++ src/components/Form/Select/SelectItem.tsx
@@ -10,13 +10,15 @@ export type SelectItemProps = SelectRadix.SelectItemProps & {
 export function SelectItem({ text, ...props }: SelectItemProps) {
   return (
     <SelectRadix.Item
-      className="flex items-center justify-between  gap-2 px-3 py-2.5 outline-none data-[highlighted]:cursor-pointer data-[highlighted]:bg-zinc-50"
+      className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[highlighted]:cursor-pointer data-[placeholder]:text-zinc-600 dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20 dark:data-[placeholder]:text-zinc-400"
       {...props}
     >
-      <SelectRadix.ItemText className="text-black">{text}</SelectRadix.ItemText>
+      <SelectRadix.ItemText asChild>
+        <span className="text-black dark:text-zinc-100">{text}</span>
+      </SelectRadix.ItemText>
 
       <SelectRadix.ItemIndicator>
-        <Check className="h-4 w-4 text-violet-500" />
+        <Check className="h-4 w-4 text-violet-500 dark:text-violet-300" />
       </SelectRadix.ItemIndicator>
     </SelectRadix.Item>
   )
--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -7,7 +7,7 @@ export type TextareaProps = ComponentProps<'textarea'>
 export function Textarea({ ...props }: TextareaProps) {
   return (
     <textarea
-      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 text-sm font-normal text-zinc-700 shadow-sm outline-none focus:border-violet-300 focus:border-violet-600 focus:ring-4 focus:ring-violet-100 focus:ring-violet-600"
+      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 text-sm font-normal text-zinc-700 shadow-sm outline-none focus:border-violet-300 focus:border-violet-600 focus:ring-4 focus:ring-violet-100 focus:ring-violet-600 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-400 dark:focus:border-violet-500 dark:focus:ring-violet-500/20"
       {...props}
     />
   )
